c98061e07fac28b1a5c5d559b5314a083981883d
darien kalist <kalistdarien8@gmail.com>
2024-10-20 19:09:21 +0200

    homepage

--- eshop_zbrane/resources/views/welcome.blade.php
+++ eshop_zbrane/resources/views/welcome.blade.php
@@ -8,22 +8,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Protest+Strike&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="app.css">
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Document</title>
     @vite('resources/css/welcome.css')
 </head>
 <body>
+
 <div class="welcome-body">
     <div class="welcome-container">
         <div class="main-text">
-            <h1 class="welcome-title">Vítejte v Gun Store</h1>
+            <h1 class="welcome-title">ZBRANĚ PRO KAŽDÉHO <br> NAJDETE ZDE VŠE POTŘEBNÉ PRO STŘELBU</h1>
         </div>
         <div class="subtitle">
-            <h2 class="welcome-subtitle">Nejlepší zbraně a doplňky na trhu</h2>
+            <h2 class="welcome-subtitle">Vyberte si z naší široké nabídky a získejte to nejlepší pro váš zážitek</h2>
         </div>
         <div class="btn">
-            <a href="#" class="welcome-button">Koupit</a>
+            <a href="#" class="welcome-button">KOUPIT</a>
         </div>
     </div>
 </div>
